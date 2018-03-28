ad92906b8d316c57591331946ce99f2c217e4357
dslztx <dslztx@gmail.com>
2018-03-29 02:12:40 +0800

    增加Druid配置

--- src/main/java/me/dslztx/booter/Booter.java
+++ src/main/java/me/dslztx/booter/Booter.java
@@ -2,6 +2,7 @@ package me.dslztx.booter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jms.activemq.ActiveMQAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 
@@ -10,9 +11,12 @@ import org.springframework.context.ApplicationContext;
  *
  * 增加“exclude=ActiveMQAutoConfiguration.class”，放弃“ActiveMQAutoConfiguration”对应的自动配置<br/>
  *
+ * 增加“exclude=DataSourceAutoConfiguration.class”，放弃“DataSourceAutoConfiguration”对应的自动配置<br/>
+ *
  * @author dslztx
  */
-@SpringBootApplication(scanBasePackages = "me.dslztx.booter", exclude = ActiveMQAutoConfiguration.class)
+@SpringBootApplication(scanBasePackages = "me.dslztx.booter", exclude = {
+    ActiveMQAutoConfiguration.class, DataSourceAutoConfiguration.class})
 public class Booter {
 
   public static void main(String[] args) {
